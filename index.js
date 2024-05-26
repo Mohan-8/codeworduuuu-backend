41e5ca4d271bdc41fd9762c9f8b88af2230641b4
Mohanakrishnan S <84438280+Mohan-8@users.noreply.github.com>
2024-05-26 13:44:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const app = express();
-
+const csrf = require("csurf");
+const cookieParser = require("cookie-parser");
 require("dotenv").config();
 
-app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+const app = express();
+const csrfProtection = csrf({ cookie: true });
 app.use(cors());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(
+  cors({
+    origin: "https://codeworduuuu-frontend.vercel.app/",
+    credentials: true,
+  })
+);
+app.use(express.json());
 
-app.get("/api", require("./routes/userRoutes"));
-app.post("/api/encrypt", require("./routes/userRoutes"));
-app.post("/api/decrypt", require("./routes/userRoutes"));
+app.set("view engine", "ejs");
+
+app.get("/", csrfProtection, (req, res) => {
+  res.render("index", { csrfToken: req.csrfToken() });
+});
+
+const userRoutes = require("./routes/userRoutes");
+
+app.use("/api", userRoutes);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
